e5bbbf7ea5876b9e8fb5aae127c77f8efa6c6a57
vdauverg <v_dauvergne@yahoo.com>
2019-03-23 16:13:34 -0400

    failure

--- src/maze.js
+++ src/maze.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
-function sketch (p) {
-    let rotation = 0;
-  
-    p.setup = function () {
-      p.createCanvas(600, 400, p.WEBGL);
-    };
-  
-    p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-      if (props.rotation){
-        rotation = props.rotation * Math.PI / 180;
-      }
-    };
-  
-    p.draw = function () {
-      p.background(100);
-      p.noStroke();
-      p.push();
-      p.rotateY(rotation);
-      p.box(100);
-      p.pop();
-    };
+function gen_maze(p) {
+  let rotation = 0;
+  var w = 25;
+  var size = 500;
+  var rc = size / w;
+  var grid = [];
+
+  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    if (props.rotation) {
+      rotation = props.rotation * Math.PI / 180;
+    }
   };
 
-class Maze extends Component {
-    
+  p.create_cell = function (x, y) {
+    p.noStroke();
+    p.fill(125, 125, 125);
+    p.rect(y * w - size / 2, x * w - size / 2, w, w);
+  };
 
-    render() {
-        return (
-            <P5Wrapper sketch={sketch} />
-        );
+  p.maze_map = function () {
+    for (var x = 0; x < rc; x++) {
+      for (var y = 0; y < rc; y++) {
+        var cell = new p.create_cell(x, y);
+        grid.push(cell);
+      }
     }
+  };
+
+  p.setup = function () {
+    p.createCanvas(size, size, p.WEBGL);
+    p.background(100);
+    p.maze_map();
+  };
+
+  p.draw = function () {
+    p.noStroke();
+    p.push();
+    //p.fill(p.random(0, 255), p.random(0, 255), p.random(0, 255));
+    p.rotateY(rotation);
+    p.pop();
+  };
+};
+
+class Maze extends Component {
+
+
+  render() {
+    return (
+      <P5Wrapper sketch={gen_maze} />
+    );
+  }
 }
 
 export default Maze;
